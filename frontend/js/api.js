eeb21389891b01ae29ddb2af47cedd26f735ad41
Drobchik Angelika <drobchik.av@edu.spbstu.ru>
2026-05-12 22:52:21 +0300

    Wrong way to call frontend, so changes for api_base_url in api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,7 +2,7 @@
 // API CONFIGURATION
 // ============================================
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = window.location.origin + '/api';
 
 // ============================================
 // API HELPER FUNCTION
